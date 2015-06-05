724098d5b431b0c2d20bc6f2433829be1118f0d8
undoxxx <undo.pc@gmail.com>
2015-06-05 18:28:36 +0900

    念のため

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UnityChan {
+	public class PlayerController : MonoBehaviour {
+
+		Animator anim;
+		UnityChanControlScriptWithRgidBody unitychan;
+
+		// Use this for initialization
+		void Start () {
+
+			anim		= GetComponent<Animator> ();
+			unitychan	= GetComponent<UnityChanControlScriptWithRgidBody> ();
+		
+		}
+		
+		// Update is called once per frame
+		void Update () {
+		
+		}
+
+		void OnGoal() {
+
+			anim.SetBool ("Win", true);
+			unitychan.enabled = false;
+
+		}
+	}
+}
